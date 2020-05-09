6ea43b941c5afd8d7b93af13eb428610073cc3bc
Jonathan Schafer <jschafer@gitlab.com>
2020-05-08 21:47:12 -0500

    Add basic CRUD for Player and Coach
    Removed Person model

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -1,4 +1,52 @@
 # frozen_string_literal: true
 
-class CoachesController < PersonsController
+class CoachesController < ApplicationController
+  def new
+    @coach = Coach.new
+  end
+
+  def create
+    @coach = Coach.new(coach_params)
+
+    if @coach.save
+      redirect_to @coach
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @coach = Coach.find(params[:id])
+  end
+
+  def index
+    @coaches = Coach.all
+  end
+
+  def show
+    @coach = Coach.find(params[:id])
+  end
+
+  def update
+    @coach = Coach.find(params[:id])
+
+    if @coach.update(coach_params)
+      redirect_to @coach
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @coach = Coach.find(params[:id])
+    @coach.destroy
+
+    redirect_to coaches_path
+  end
+
+  private
+
+  def coach_params
+    params.require(:coach).permit(:name, :role)
+  end
 end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,52 @@
 # frozen_string_literal: true
 
-class PlayersController < PersonsController
+class PlayersController < ApplicationController
+  def new
+    @player = Player.new
+  end
+
+  def create
+    @player = Player.new(player_params)
+
+    if @player.save
+      redirect_to @player
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @player = Player.find(params[:id])
+  end
+
+  def index
+    @players = Player.all
+  end
+
+  def show
+    @player = Player.find(params[:id])
+  end
+
+  def update
+    @player = Player.find(params[:id])
+
+    if @player.update(player_params)
+      redirect_to @player
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @player = Player.find(params[:id])
+    @player.destroy
+
+    redirect_to players_path
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name, :position)
+  end
 end
