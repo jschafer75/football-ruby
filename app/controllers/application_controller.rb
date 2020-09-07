3f4e30aef9a9ea60bd208322a7e14be3370ebe82
Jonathan Schafer <jschafer@gitlab.com>
2020-09-06 22:42:31 -0500

    Connect User to Franchise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(_resource)
+    current_user.franchise ? franchise_path(current_user.franchise) : franchises_path
+  end
 end
