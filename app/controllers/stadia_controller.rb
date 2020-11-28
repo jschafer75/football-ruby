f2c73980c0b5e5dcdbaecd89b3f8986360bffed8
Jonathan Schafer <jschafer@gitlab.com>
2020-11-28 14:57:04 -0600

    Facility and Stadium upgrading
    Remove Stadium enum
    Add cost for upgrading
    Add max level for upgrades

--- app/controllers/stadia_controller.rb
+++ app/controllers/stadia_controller.rb
@@ -1,4 +1,30 @@
 # frozen_string_literal: true
 
 class StadiaController < ApplicationController
+  before_action :set_stadium, only: %i[show edit update destroy upgrade]
+  before_action :set_franchise, only: %i[show edit update destroy upgrade]
+
+  # PATCH/PUT /stadium/1/upgrade
+  # PATCH/PUT /stadium/1/upgrade.json
+  def upgrade
+    @stadium.upgrade
+
+    respond_to do |format|
+      format.html do
+        redirect_to @franchise, notice: 'Stadium was successfully upgraded.'
+      end
+      format.json { render :show, status: :ok, location: @franchise }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_stadium
+    @stadium = Stadium.find(params[:id])
+  end
+
+  def set_franchise
+    @franchise = Franchise.find(params[:franchise_id])
+  end
 end
