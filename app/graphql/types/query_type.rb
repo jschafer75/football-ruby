738c6726f22d9330e338681c0b3d6fbc762fa457
Jonathan Schafer <jschafer@gitlab.com>
2021-03-28 22:35:23 -0500

    Add Franchise GraphQL type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -9,11 +9,13 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-                               description: 'An example field added by the generator'
-    def test_field
-      'Hello World!'
+    field :franchises,
+          [Types::FranchiseType],
+          null: false,
+          description: 'Returns a list of franchises'
+
+    def franchises
+      Franchise.all
     end
   end
 end
